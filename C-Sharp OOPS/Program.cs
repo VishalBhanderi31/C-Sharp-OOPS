706abd8e0a40ba8508c6fb4dda4f1f3a000606fb
iVishal <iVishal@DESKTOP-UF3GTLO>
2020-05-13 17:51:36 +0530

    Stack Example by object

--- C-Sharp OOPS/Program.cs
+++ C-Sharp OOPS/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 namespace C_Sharp_OOPS
 {
     //its like a power-point application
+
     class Program
     {
         static void Main(string[] args)
@@ -31,14 +33,34 @@ namespace C_Sharp_OOPS
             //var car = new Car("XYZ1234");
 
             //Upcasting and Downcasting
-            var textOne = new TextOne();
-            Shape shape = textOne;
+            //var textOne = new TextOne();
+            //Shape shape = textOne;
+
+            var stack = new Stack();
+            const int startPoint = 1;
+            const int endPoint = 5;
 
 
+            for (var i = startPoint; i <= endPoint; i++)
+            {
+                stack.Push(i);
+            }
 
+            stack.Clear();
+            
 
+            for (var j = startPoint; j <= endPoint; j++)
+            {
+                Console.WriteLine(stack.Pop());
+            }
 
+            stack.Push(0);
+            stack.Push(2);
+            stack.Push(3);
 
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
         }
     }
 }
---
+++ C-Sharp OOPS/Stack/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_OOPS
+{
+    public class Stack
+    {
+        private readonly List<object> _stack;
+
+        public Stack()
+        {
+            _stack = new List<object>();
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("Null object is not allowed.");
+            
+            _stack.Add(obj);
+
+        }
+
+
+        public object Pop()
+        {
+            if(_stack.Count == 0)
+                throw new InvalidOperationException("An empty stack can not pop element.");
+
+            var index = _stack.Count - 1;
+            var object1 = _stack[index];
+            _stack.RemoveAt(index);
+            return object1;
+
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
