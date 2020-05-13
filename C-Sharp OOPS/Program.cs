ec621291cdf994d788233d477d7feb98b5609715
iVishal <iVishal@DESKTOP-UF3GTLO>
2020-05-13 09:08:47 +0530

    Adding Individual Class testing comment

--- C-Sharp OOPS/Program.cs
+++ C-Sharp OOPS/Program.cs
@@ -9,16 +9,19 @@ namespace C_Sharp_OOPS
     {
         static void Main(string[] args)
         {
+            //Test Inheritance
             //var text = new Text {Width = 10f}; // Here we use ObjectInitializer to use Width property
             //text.Copy();
 
-            var dbMigrator = new DbMigrator(new Logger());
+
+            //Test Composition
+            /*var dbMigrator = new DbMigrator(new Logger());
 
             var logger = new Logger();
             var installer = new Installer(logger);
 
             dbMigrator.Migrate();
-            installer.Install();
+            installer.Install();*/
 
 
 
